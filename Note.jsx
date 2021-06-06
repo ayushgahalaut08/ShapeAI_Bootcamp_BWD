a56a44c6e6b662a72e86ee65cb3fe3c240d36d55
ayushgahalaut08 <85450404+ayushgahalaut08@users.noreply.github.com>
2021-06-06 21:42:54 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web Development with React JS</h1>
+      <p>
+        The bootcamp was conducted by Shaurya Sir from ShapeAI . It was a 7 days
+        free bootcamp on youtube and required no prerequistes and involved
+        everything from scratch. It covered basics of web designing including
+        HTML , JAVA and REACT.JS.
+      </p>
+    </div>
+  );
+}
+export default Note;
